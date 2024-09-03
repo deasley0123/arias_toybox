aeb82df6e07f47b3bab8ec66d17cf81f0328a388
David Easley <7064388+deasley0123@users.noreply.github.com>
2024-09-03 12:25:42 +1200

    Finish(?) txt conversion script. Pipe in scene1

--- convertTxtToRpy.py
+++ convertTxtToRpy.py
@@ -1,3 +1,6 @@
+def printRenPyLine(name, card):
+    print(name[0] + " " + '"' + card + '"')
+
 def read_file_line_by_line(file_path):
     unknownName = ("q", "???")
     narrator = ("n", "")
@@ -7,18 +10,38 @@ def read_file_line_by_line(file_path):
         with open(file_path, 'r') as file:
             # Iterate over each line in the file
             name = unknownName
+            card = ""
+            isFirstLine = True
+
             for line in file:
                 curLine = line
+                # IF the current line is a dialogue tag Name, then...
                 if curLine[0] == '#':
-                    #We've got ourselves a new name card
+                    # Print out current RenPy line
+                    if not isFirstLine:
+                        printRenPyLine(name, card)
+                    isFirstLine = False
+                    # Strip off the leading character, trim whitespace
                     curLine = curLine.lstrip('#').strip()
-                    if curLine.isalnum():
-                        name = (curLine[0].lower(), curLine)
-                    else:
+                    
+                    # Configure the new name
+                    if curLine.isalnum() != True:
                         name = unknownName
-                    print(name)
+                    elif curLine == "Narration":
+                        name = narrator
+                    else:
+                        name = (curLine[0].lower(), curLine)
+                    # print(name)
+                    card = ""
+                # IF the current line is a part of the dialogue, then...
                 else:
-                    print(curLine.strip())
+                    if card == "":
+                        card = curLine.strip()
+                    else:
+                        card += "{p}" + curLine.strip()
+                    # print(card)
+            # Print out current RenPy line
+            printRenPyLine(name, card)
     except FileNotFoundError:
         print(f"The file at {file_path} was not found.")
     except IOError as e:
